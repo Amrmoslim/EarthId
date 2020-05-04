91d137d12e5ca0437255b370e663142c0fed9d0a
Amr Moslim <moslimson@gmail.com>
2020-05-04 21:31:44 +0200

    Update Production Profile Estimation2.R

--- Production Profile Estimation2.R
+++ Production Profile Estimation2.R
@@ -4,7 +4,7 @@ Duration = 10
 DR <- 0.2
 df <- NULL
 
-for (i in 0:Duration) {
+for (i in 1:Duration) {
   
   Pi = Pi- (Pi*DR)
   print(paste(round(Pi,0),i+1))
@@ -16,7 +16,8 @@ names(df)[1] <- "Time"
 names(df)[2] <- "Production"
 
 df
-
+#df <- rbind(df,data.frame("Time"=1,"Production"=Pi))
+#df
 plot(df, ylab= "Production(STB)", xlab = "Time in years")
 
 print(paste('the total production is' , sum(df$Production)*365))
@@ -42,6 +43,5 @@ prod.fun(1000,10,0.2)
 print(df)
 
 ######------------------------------------
-prod.fun(1000,10,0.2
-         )
+prod.fun(1000,10,0.2)
 
